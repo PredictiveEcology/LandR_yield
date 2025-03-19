fe98a4bcc01b82b75dd5e4df7d6b8476088548c5
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-03-19 08:14:07 -0700

    biomass_yieldTables 2.0

--- global.R
+++ global.R
@@ -6,19 +6,24 @@ if (!require("SpaDES.project")){
 
 out <- SpaDES.project::setupProject(
   Restart = TRUE,
-  paths = list(projectPath = getwd()),
+  paths = list(projectPath = getwd(),
+               inputPath = "inputs",
+               outputPath = "outputs",
+               cachePath = "cache"),
   options = options(spades.moduleCodeChecks = FALSE,
                     spades.recoveryMode = FALSE),
   modules = c("PredictiveEcology/Biomass_speciesFactorial@development",
               "PredictiveEcology/Biomass_borealDataPrep@development",
               "PredictiveEcology/Biomass_speciesParameters@development",
-              "PredictiveEcology/Biomass_yieldTables@main"
+              "DominiqueCaron/Biomass_yieldTables@reorganisation"
   ),
   params = list(
     .globals = list(
-      sppEquivCol = LandR::equivalentNameColumn(
-        c("Abie_las", "Betu_pap", "Pice_gla", "Pice_mar", "Pinu_con", "Popu_tre",
-          "Pice_eng"), LandR::sppEquivalencies_CA), ".studyAreaName" = "RIA"
+      dataYear = 2011, #will get kNN 2011 data, and NTEMS 2011 landcover
+      .plots = c("png"),
+      .plotInterval = 10,
+      sppEquivCol = 'LandR',
+      .studyAreaName = "RIA"
     ),
     Biomass_borealDataPrep = list(
       .studyAreaName = "RIA",
@@ -36,38 +41,41 @@ out <- SpaDES.project::setupProject(
       speciesFittingApproach = "focal"
     ),
     Biomass_yieldTables = list(
-      moduleNameAndBranch = "PredictiveEcology/Biomass_core@development (>= 1.3.9)",
+      moduleNameAndBranch = "PredictiveEcology/Biomass_core@main",
       .plots = "png",
       .useCache = "generateData"
     )
   ),
   packages = c("googledrive", 'RCurl', 'XML', "stars"),
-  useGit = T,
+  useGit = F,
   functions = "R/getRIA.R",
   # Study area is RIA
   studyArea = {
-    reproducible::prepInputs(url = "https://drive.google.com/file/d/1LxacDOobTrRUppamkGgVAUFIxNT4iiHU/view?usp=sharing",
-                             destinationPath = "inputs",
-                             fun = getRIA,
-                             overwrite = TRUE)
+    reproducible::prepInputs(
+      url = "https://drive.google.com/file/d/1LxacDOobTrRUppamkGgVAUFIxNT4iiHU/view?usp=sharing",
+      destinationPath = "inputs",
+      fun = getRIA,
+      overwrite = TRUE
+    )
   },
   studyAreaLarge = studyArea,
-  speciesToUse = c("Abie_las", "Betu_pap", "Pice_gla", "Pice_mar", "Pinu_con",
-                   "Popu_tre", "Pice_eng"),
-  speciesNameConvention = LandR::equivalentNameColumn(speciesToUse, LandR::sppEquivalencies_CA),
-  sppEquiv = {
-    sppEquiv <- LandR::sppEquivalencies_CA[LandR::sppEquivalencies_CA[[speciesNameConvention]] %in% speciesToUse,]
-    sppEquiv
+  rasterToMatch = {
+    targetCRS <- terra::crs(studyArea)
+    rtm <- terra::rast(studyArea, res = c(250, 250), crs = targetCRS)
+    rtm[] <- 1
+    rtm <- terra::mask(rtm, studyArea)
+    rtm
   },
-  sppColorVect = {
-    sppColorVect <- LandR::sppColors(sppEquiv, speciesNameConvention, palette = "Set1")
-    sppNames <- names(sppColorVect)
-    sppColorVect <- RColorBrewer::brewer.pal(name = "Set1", 8)
-    names(sppColorVect) <- c(sppNames, "Mixed")
-    sppColorVect
+  sppEquiv = {
+    speciesInStudy <- LandR::speciesInStudyArea(studyAreaLarge,
+                                                dPath = "inputs")
+    species <- LandR::equivalentName(speciesInStudy$speciesList, df = LandR::sppEquivalencies_CA, "LandR")
+    sppEquiv <- LandR::sppEquivalencies_CA[LandR %in% species]
+    sppEquiv <- sppEquiv[KNN != "" & LANDIS_traits != ""] #avoid a bug with shore pine
   }
 )
 
 out$loadOrder <- unlist(out$modules)
 
-simOut <- SpaDES.core::simInitAndSpades2(out)
+initOut <- SpaDES.core::simInit2(out)
+simOut <- SpaDES.core::spades(initOut)
